61b84a97aac944a9d4e86b1fc787043d1a0b0e7a
Continous <continous159@gmail.com>
2026-08-12 17:08:25 -0700

    Updated hello world to reflect newfound purpose.

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@ F4SE_PLUGIN_LOAD(const F4SE::LoadInterface* a_f4se)
 {
 	F4SE::Init(a_f4se);
 
-	REX::INFO("Hello World!");
+	REX::INFO("Hello World! I am the (F)allout (4) (FFMPEG) plugin.");
 
 	return true;
 }
