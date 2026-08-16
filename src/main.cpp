97825945e32ccac23a15c487571f5b6591fec1cf
Continous <continous159@gmail.com>
2026-08-15 22:36:13 -0700

    Add FFMPEG version echo

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
+extern "C"
+{
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavutil/avutil.h>
+}
+
 F4SE_PLUGIN_LOAD(const F4SE::LoadInterface* a_f4se)
 {
-	F4SE::Init(a_f4se);
+    F4SE::Init(a_f4se);
 
-	REX::INFO("Hello World! I am the (F)allout (4) (FFMPEG) plugin.");
+    REX::INFO("Hello World! I am the (F)allout (4) (FFMPEG) plugin.");
+    REX::INFO("FFmpeg version: {}", av_version_info());
 
-	return true;
+    return true;
 }
