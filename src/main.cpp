f9ade173555ba700720cf299db1afd04f18169b1
Continous <continous159@gmail.com>
2026-08-15 23:41:36 -0700

    Initialize decoder code, and test pass in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,25 @@ extern "C"
 #include <libavutil/avutil.h>
 }
 
+#include "decoder.h"
+
 F4SE_PLUGIN_LOAD(const F4SE::LoadInterface* a_f4se)
 {
     F4SE::Init(a_f4se);
 
     REX::INFO("Hello World! I am the (F)allout (4) (FFMPEG) plugin.");
     REX::INFO("FFmpeg version: {}", av_version_info());
+
+    f4ffmpeg::decoder testDecoder;
+
+    if (testDecoder.open("Data/Video/MainMenuLoop.bk2"))
+    {
+        REX::INFO("Successfully opened intro video.");
+    }
+    else
+    {
+        REX::ERROR("Failed to open intro video. FFMPEG is presumed non-functional.");
+    }
 
     return true;
 }
