5e6daf383e8a53500e63b7919497d349dc8a2529
Continous <continous159@gmail.com>
2026-08-15 23:42:48 -0700

    Forgot files lmao

---
+++ src/decoder.cpp
@@ -0,0 +1,45 @@
+#include "decoder.h"
+
+namespace f4ffmpeg
+{
+        bool decoder::open(const char* path)
+    {
+        if (formatContext != nullptr)
+        {
+            close();
+        } //If there is not formatContext provided we can just close out. There's presumably no media being provided.
+
+        REX::INFO("Attempting to open media: {}", path);
+
+        if (avformat_open_input(
+                &formatContext,
+                path,
+                nullptr,
+                nullptr) < 0)
+        {
+            formatContext = nullptr; //Release formatContext, we failed to open.
+
+            REX::ERROR("FFmpeg failed to open media: {}", path);
+
+            return false;
+        }
+
+        if (avformat_find_stream_info(
+                formatContext,
+                nullptr) < 0)
+        {
+            REX::ERROR("FFmpeg failed to read stream information: {}", path);
+
+            close();
+
+            return false;
+        }
+
+        REX::INFO(
+            "FFmpeg opened media successfully. Stream count: {}",
+            formatContext->nb_streams
+        );
+
+        return true;
+    }
+}
---
+++ src/decoder.h
@@ -0,0 +1,19 @@
+#pragma once
+
+extern "C"
+{
+#include <libavformat/avformat.h>
+}
+
+namespace f4ffmpeg
+{
+    class decoder
+    {
+    public:
+        bool open(const char* path);
+        void close();
+
+    private:
+        AVFormatContext* formatContext = nullptr;
+    };
+}
