416044ee915f9e2fbd392d4d91a887821c219dc3
Continous <continous159@gmail.com>
2026-08-28 17:45:24 -0700

    Delete src/placeboLibav.c

--- src/placeboLibav.c
+++
@@ -1,4 +0,0 @@
-// libplacebo's FFmpeg interoperability helpers are implemented as a single
-// header. They intentionally need exactly one C translation unit with this set.
-#define PL_LIBAV_IMPLEMENTATION 1
-#include <libplacebo/utils/libav.h>
